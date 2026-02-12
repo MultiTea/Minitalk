854f0aa8b08d6ab83f0fc88fd3e051c4854d3fea
MultiTea <lucasbolea@live.fr>
2026-02-12 19:36:51 +0100

    Testing out the client and server logic

--- src/client.c
+++ src/client.c
@@ -6,19 +6,58 @@
 /*   By: lbolea <lbolea@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/11 11:21:36 by lbolea            #+#    #+#             */
-/*   Updated: 2026/02/11 17:08:25 by lbolea           ###   ########.fr       */
+/*   Updated: 2026/02/12 14:15:55 by lbolea           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minitalk.h"
 
-int	main(__pid_t svr_pid, char **msg)
+int	main(int argc, char **argv)
 {
-	// 1. Emcrypt message
-	// 2. Send message to PID
-	// 3. Stop communication
-	(void)svr_pid;
-	(void)msg;
-	printf("CLIENT");
+	pid_t	srv_pid;
+	int		str_len;
+	int		i;
+	int		j;
+	int		pos;
+
+	i = 2;
+	srv_pid = ft_atoi(argv[1]);
+	while (i < argc)
+	{
+		str_len = ft_strlen(argv[i]);
+		j = 0;
+		while (j != str_len)
+		{
+			pos = 7;
+			while (pos > -1)
+			{
+				if (((argv[i][j] >> pos) & 1) == 1)
+				{
+					kill(srv_pid, SIGUSR2);
+					usleep(25);
+				}
+				else
+				{
+					kill(srv_pid, SIGUSR1);
+					usleep(25);
+				}
+				pos--;
+			}
+			j++;
+		}
+		pos = 7;
+		if (j == str_len)
+		{
+			while (pos > -1)
+			{
+				kill(srv_pid, SIGUSR1);
+				usleep(25);
+				pos--;
+			}
+			i++;
+		}
+		ft_printf("END OF MESSAGE");
+		kill(getpid(), SIGKILL);
+	}
 	return (0);
 }
--- src/server.c
+++ src/server.c
@@ -6,7 +6,7 @@
 /*   By: lbolea <lbolea@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/11 11:25:37 by lbolea            #+#    #+#             */
-/*   Updated: 2026/02/12 12:17:10 by lbolea           ###   ########.fr       */
+/*   Updated: 2026/02/12 14:21:14 by lbolea           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,28 +27,20 @@ void	print_bit(char g_bit)
 	ft_printf("\n");
 }
 
-void	stop_prog(void)
-{
-	ft_printf("\n");
-	print_bit(g_bit);
-	ft_printf("%c\n", g_bit);
-	g_bit = '\0';
-	kill(getpid(), SIGSTOP);
-}
-
 void	handler(int signum)
 {
 	static int	pos = 7;
 
-	ft_printf("%d", pos);
 	if (signum == SIGUSR2)
 		g_bit = g_bit | (1 << pos);
 	pos--;
 	if (pos == -1)
 	{
+		if (g_bit == '\0')
+			g_bit = '\0';
 		write(1, &g_bit, 1);
 		pos = 7;
-		exit(EXIT_SUCCESS);
+		g_bit = 0;
 	}
 }
 
@@ -56,18 +48,13 @@ int	main(void)
 {
 	pid_t				pid;
 	struct sigaction	sa;
-	int					pos;
-	unsigned char		bit_to_copy;
 
 	g_bit = '\0';
 	pid = getpid();
 	sa.sa_handler = &handler;
 	sa.sa_flags = SA_RESTART;
 	(void)g_bit;
-	ft_printf("%d\n", pid);
-	scanf("%c", &bit_to_copy);
-	print_bit(bit_to_copy);
-	pos = 7;
+	ft_printf("PID : %d\n", pid);
 	sigaction(SIGUSR1, &sa, NULL);
 	sigaction(SIGUSR2, &sa, NULL);
 	while (pause())
